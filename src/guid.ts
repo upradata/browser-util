e5d377242620bb91d8bb88b07d3790ee1ce2003a
Thomas Milotti <thomas.milotti@gmail.com>
2019-08-07 00:04:58 +0200

    Crypto correction

--- src/guid.ts
+++ src/guid.ts
@@ -1,7 +1,8 @@
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+const cryptoo = 'crypto' in this ? crypto : require('crypto');
 
 export function guid() {
     return ([ 1e7 ] as any + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[ 0 ] & 15 >> c / 4).toString(16)
+        (c ^ cryptoo.getRandomValues(new Uint8Array(1))[ 0 ] & 15 >> c / 4).toString(16)
     );
 }
