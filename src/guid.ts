28f3371455a33198648882f40ddf9ef7e50fe16e
Thomas Milotti <thomas.milotti@gmail.com>
2019-08-07 01:03:16 +0200

    Finally fix guid.ts

--- src/guid.ts
+++ src/guid.ts
@@ -1,8 +1,9 @@
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-const cryptoo = typeof window !== 'undefined' ? crypto : require('crypto');
+const isBrowser = typeof window !== 'undefined';
+const random = isBrowser ? crypto.getRandomValues : require('crypto').randomFillSync;
 
 export function guid() {
     return ([ 1e7 ] as any + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ cryptoo.getRandomValues(new Uint8Array(1))[ 0 ] & 15 >> c / 4).toString(16)
+        (c ^ random(new Uint8Array(1))[ 0 ] & 15 >> c / 4).toString(16)
     );
 }
