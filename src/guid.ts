942e81891b143ad4809f2120699c2e4ed906068c
Thomas Milotti <thomas.milotti@gmail.com>
2019-08-07 00:30:19 +0200

    Fix crypto in guid.ts

--- src/guid.ts
+++ src/guid.ts
@@ -1,5 +1,5 @@
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-const cryptoo = 'crypto' in this ? crypto : require('crypto');
+const cryptoo = typeof window === 'undefined' ? crypto : require('crypto');
 
 export function guid() {
     return ([ 1e7 ] as any + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
