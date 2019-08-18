3e6005b899bb757515c72a0d735218b21b5654ba
Thomas Milotti <thomas.milotti@gmail.com>
2019-08-18 21:13:09 +0200

    guid.ts Webpack ENVIRONEMENT

--- src/guid.ts
+++ src/guid.ts
@@ -1,9 +1,18 @@
 declare var ENVIRONMENT: 'browser' | 'node';
 
-// https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-const isBrowser = typeof window !== 'undefined' || (typeof ENVIRONMENT !== 'undefined' && ENVIRONMENT === 'browser');
+type Random = <T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView>(array: T) => T;
 
-const random = isBrowser ? crypto.getRandomValues : require('crypto').randomFillSync;
+let random: Random = undefined;
+
+if (typeof ENVIRONMENT !== 'undefined' && ENVIRONMENT === 'browser')
+    random = crypto.getRandomValues;
+else {
+    // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+    const isBrowser = typeof window !== 'undefined';
+    random = isBrowser ? crypto.getRandomValues : require('crypto').randomFillSync;
+}
+
+// const random = isBrowser ? crypto.getRandomValues : require('crypto').randomFillSync;
 
 export function guid() {
     return ([ 1e7 ] as any + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
