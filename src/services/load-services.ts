3a5c661fe9ffbbcf01a628c756c239decfc33abc
Thomas Milotti <thomas.milotti@gmail.com>
2020-09-22 09:33:33 +0200

    Enhanced load-services

--- src/services/load-services.ts
+++ src/services/load-services.ts
@@ -1,28 +1,40 @@
 import { dispatchCustomEvent } from '../custom-events';
-import { Service, LoadServicesModule, Services, ServicesConfig } from './types';
+import { LoadModuleServices, ModulesServices, ModulesServicesConfig } from './types';
+import { ReplaySubject, Observable } from 'rxjs';
 import { entries, ObjectOf, assignRecursive, AssignOptions, Function1 } from '@upradata/util';
 
+type Services = any;
 
-export async function loadServices<S extends Services>(servicesConfig?: Partial<ServicesConfig<S>>): Promise<Partial<S>> {
+let resolve: Function1<Services> = undefined;
+const loaded$ = new ReplaySubject<Services>(1);
+
+const servicesPromise = new Promise<Services>((res, _rej) => resolve = res);
+
+export const servicesPromise$ = <Services>(): Promise<Services> => servicesPromise;
+export const servicesObs$ = <Services>(): Observable<Services> => loaded$.asObservable();
+
+
+
+export async function loadServices<M extends ModulesServices<S>, S = any>(modulesServicesConfig?: Partial<ModulesServicesConfig<M, S>>): Promise<Partial<M>> {
 
     const { windowGlobal, include, exclude, dispatchEvents, servicesLoadedEventName, serviceLoadedEventName } =
-        assignRecursive(new ServicesConfig(), servicesConfig, new AssignOptions({ arrayMode: 'replace' }));
+        assignRecursive(new ModulesServicesConfig(), modulesServicesConfig, new AssignOptions({ arrayMode: 'replace' }));
 
     const services = {};
 
-    const loadedPromises: Promise<{ name: string; services: ObjectOf<Service>; }>[] = [];
+    const loadedPromises: Promise<{ name: string; services: ObjectOf<S>; }>[] = [];
 
-    const addService = async (name: string, config: any, module$: Promise<LoadServicesModule<ObjectOf<Service>>>) => {
+    const addService = async (name: string, config: any, module$: Promise<LoadModuleServices<any, ObjectOf<S>, S>>) => {
         const loaded = module$.then(m => m.loadServices(config)).then(services => ({ name, services }));
         loadedPromises.push(loaded);
     };
 
-    for (const [ name, serviceConfig ] of entries(servicesConfig.services)) {
+    for (const [ name, serviceConfig ] of entries(modulesServicesConfig.modulesServices)) {
         if (exclude && exclude[ name ])
             continue;
 
         if (!include || include[ name ])
-            addService(name as string, serviceConfig.config, Promise.resolve(serviceConfig.loadServicesModule) || import(serviceConfig.path));
+            addService(name as string, serviceConfig.config, Promise.resolve(serviceConfig.module) /* || import(serviceConfig.path) */);
     }
 
     const servicesLoaded = await Promise.all(loadedPromises);
@@ -47,5 +59,8 @@ export async function loadServices<S extends Services>(servicesConfig?: Partial<
     if (dispatchEvents)
         dispatchCustomEvent(servicesLoadedEventName);
 
+    resolve(services);
+    loaded$.next(services);
+
     return services;
-};
+}
--- src/services/types.ts
+++ src/services/types.ts
@@ -1,32 +1,39 @@
-export type Service<BaseService = any> = BaseService | any;
+// export type Service<BaseService = any> = BaseService | any;
 
 
-export type Services<T = { [ k: string ]: any; }> = {
+/* export type ModuleServices<T = { [ k: string ]: any; }> = {
     [ key in keyof T ]: Service;
-};
+}; */
 
-
-export interface LoadServices<S extends Services> {
-    (configuration?: Partial<ServicesConfig<S>>): Promise<Partial<S>>;
+export interface ModuleServices<Service> {
+    [ key: string ]: Service;
 }
 
-export interface LoadServicesModule<S extends Services> {
-    loadServices: LoadServices<S>;
+export interface LoadServices<C, M extends ModuleServices<S>, S = any> {
+    (configuration?: C): Promise<M>;
+}
+
+export interface LoadModuleServices<C, M extends ModuleServices<any>, S = any> {
+    loadServices: LoadServices<C, M, S>;
 }
 
 
-export interface ServiceConfig<C = any, S extends Services = Services> {
-    path?: string;
-    loadServicesModule?: LoadServicesModule<S> | Promise<LoadServicesModule<S>>;
+export interface ModuleServicesConfig<M extends ModuleServices<any> = ModuleServices<any>, C = any, S = any> {
+    // path?: string; => import(path) will not work with webpack. It does not know what is path during building
+    module?: LoadModuleServices<C, M, S> | Promise<LoadModuleServices<C, M, S>>;
     config?: C;
 }
 
-export class ServicesConfig<Services> {
-    services: Partial<Record<keyof Services, ServiceConfig>>;
+export interface ModulesServices<Service> {
+    [ module: string ]: ModuleServices<Service>;
+}
+
+export class ModulesServicesConfig<Modules extends ModulesServices<Service>, Service = any> {
+    modulesServices: Partial<Record<keyof Modules, ModuleServicesConfig>>;
     windowGlobal: string;
-    include: Partial<Record<keyof Services, boolean>>;
-    exclude: Partial<Record<keyof Services, boolean>>;
+    include: Partial<Record<keyof Modules, boolean>>;
+    exclude: Partial<Record<keyof Modules, boolean>>;
     dispatchEvents: boolean = true;
     servicesLoadedEventName: string = '__services-loaded__';
     serviceLoadedEventName: (name: string) => string = name => `__services-loaded__/${name}`;
-};
+}
